c685c47c16d41c056b215663cbe036b9dcf91fea
wyatthumphreys <wlhumphreys@student.rtc.edu>
2018-09-28 00:12:25 -0700

    Final Commit

--- Fall2018/DebugLab/src/LetterCounter.java
+++ Fall2018/DebugLab/src/LetterCounter.java
@@ -3,19 +3,17 @@
     CNA337
     Zachary Rubin zrubin@rtc.edu
  */
-
+import java.util.Scanner;
 class LetterCounter {
+
   public static void main (String[] args) {
     int LetterCount = 0;
-    String check_word = args[0];
+    Scanner scanner = new Scanner(System.in);  // Reading from System.in
+    System.out.println("Enter word: ");
+    String check_word = scanner.next();
     String single_letter = "";
-    int i;
-    for (i = 0; i< check_word.length(); i++) {
-      single_letter = check_word.substring(1, 1);
-      if (single_letter.equals("g")) {
-        LetterCount++;
-      }
-    }
-    System.out.println("G was found " + LetterCount + " times in the word " + check_word);
+
+    System.out.print("The length of your word is: " );
+    System.out.println(check_word.length());
   }
 }
